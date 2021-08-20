75bb6ced3ad8af95f97b3f406af06629000e1d42
Jack Lim <jennystr233@gmail.com>
2021-08-21 02:11:51 +0900

    add transition

--- capa/src/components/Header.js
+++ capa/src/components/Header.js
@@ -1,11 +1,14 @@
 import { BankOutlined, MenuOutlined } from '@ant-design/icons';
 import "../style/Header.css";
 import React, { useState } from "react";
+import { $CombinedState } from 'redux';
+import $ from "jquery";
 
 const Header = () => {
   const [slider, setSlider] = useState(false);
+  const [hide, setHide] = useState(false);
+  
   const handleSlider = () => {
-    //console.log("작동완료")
     if(slider){
       setSlider(!slider);
     } else {
@@ -13,11 +16,27 @@ const Header = () => {
     }
   }
 
+  $('#mainContents').click((e) => {
+    if(!$(e.target).hasClass("area")) {
+      console.log("됨?")
+    }
+  })
+
   return (
     <>
       <div id="header">
         <div className="leftHeaderContents">
           <MenuOutlined className="mobileMenuBtn" onClick={handleSlider}/>
+          {slider ? <div className="mobileSliderBtn">
+            <div className="logoPositioning">
+              <div className="mobileLOGOIMG"> CAPA </div>
+              <div className="mobileLOGO"> 파트너스 </div>
+            </div>
+            <div className="belowInfo">
+              <div><BankOutlined />파트너정밀가공</div>
+              <div>로그아웃</div>
+            </div>
+          </div> : <></>}
           <div className="capaLogo"> CAPA </div>
           <div className="capaLogoName"> 파트너스 </div>
         </div>
